ca13abacf84a9ec9b50de0d4977cdbdc774e13bc
Bryce Cahill <brycec@invenco.com>
2023-06-12 16:59:20 +1200

    HACK: Support non-annotated bytes types

--- bytechomp/serialization.py
+++ bytechomp/serialization.py
@@ -144,10 +144,19 @@ def flatten_dataclass(data_object: type) -> tuple[str, list[int | float | bytes]
 
             else:
                 raise Exception(f"unsupported annotated type: {arg_type} (field: {field.name})")
-        elif field.type in [list, bytes]:
+        elif field.type in [list]:
             raise Exception(
                 f"annotation needed for list/bytes (length required, field: {field.name})"
             )
+        elif field.type in [bytes]:
+            # Handle non-annotated bytes type
+            # @TODO: Abstract bytes and lists nicely to remove annotation req
+            if not isinstance(val, bytes):
+                raise TypeError(
+                    f"{field.name} field contains {val_t} type but requires {field.type}")
+
+            pattern += f"{len(val)}s"
+            values.append(val)
         else:
             raise Exception(f"unsupported data type ({field.type}) on field {field.name}")
 
